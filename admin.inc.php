cd1a97ef0f1c1b5b5014613197c0e6d42a83827a
Ajay <Ajay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-23 13:57:29 +0000

    git-svn-id: http://plugins.svn.wordpress.org/better-search/trunk@104428 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin.inc.php
+++ admin.inc.php
@@ -2,6 +2,10 @@
 /**********************************************************************
 *					Admin Page										*
 *********************************************************************/
+if (!defined('ABSPATH')) die("Aren't you supposed to come here via WP-Admin?");
+
+if (!defined('BSEARCH_LOCAL_NAME')) define('BSEARCH_LOCAL_NAME', 'better-search');
+
 // Pre-2.6 compatibility
 if ( !defined('WP_CONTENT_URL') )
 	define( 'WP_CONTENT_URL', get_option('siteurl') . '/wp-content');
@@ -25,6 +29,7 @@ function bsearch_options() {
 		$bsearch_settings[limit] = ($_POST['limit']);
 		$bsearch_settings[use_fulltext] = (($_POST['use_fulltext']) ? true : false);
 		$bsearch_settings[d_use_js] = (($_POST['d_use_js']) ? true : false);
+		$bsearch_settings[include_pages] = (($_POST['include_pages']) ? true : false);
 		$bsearch_settings[show_credit] = (($_POST['show_credit']) ? true : false);
 		
 		$bsearch_settings[heatmap_smallest] = ($_POST['heatmap_smallest']);
@@ -37,7 +42,7 @@ function bsearch_options() {
 		
 		update_option('ald_bsearch_settings', $bsearch_settings);
 		
-		$str = '<div id="message" class="updated fade"><p>'. __('Options saved successfully.','ald_bsearch_plugin') .'</p></div>';
+		$str = '<div id="message" class="updated fade"><p>'. __('Options saved successfully.', BSEARCH_LOCAL_NAME) .'</p></div>';
 		echo $str;
 	}
 	
@@ -46,7 +51,7 @@ function bsearch_options() {
 		$bsearch_settings = bsearch_default_options();
 		update_option('ald_bsearch_settings', $bsearch_settings);
 		
-		$str = '<div id="message" class="updated fade"><p>'. __('Options set to Default.','ald_bsearch_plugin') .'</p></div>';
+		$str = '<div id="message" class="updated fade"><p>'. __('Options set to Default.', BSEARCH_LOCAL_NAME) .'</p></div>';
 		echo $str;
 	}
 ?>
@@ -57,113 +62,119 @@ function bsearch_options() {
     <fieldset class="options">
     <legend>
     <h3>
-      <?php _e('Support the Development','ald_bsearch_plugin'); ?>
+      <?php _e('Support the Development', BSEARCH_LOCAL_NAME); ?>
     </h3>
     </legend>
     <p>
-      <?php _e('If you find ','ald_bsearch_plugin'); ?>
+      <?php _e('If you find ', BSEARCH_LOCAL_NAME); ?>
       <a href="http://ajaydsouza.com/wordpress/plugins/better-search/">Better Search</a>
-      <?php _e('useful, please do','ald_bsearch_plugin'); ?>
-      <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&amp;business=donate@ajaydsouza.com&amp;item_name=Better%20Search%20(From%20WP-Admin)&amp;no_shipping=1&amp;return=http://ajaydsouza.com/wordpress/plugins/better-search/&amp;cancel_return=http://ajaydsouza.com/wordpress/plugins/better-search/&amp;cn=Note%20to%20Author&amp;tax=0&amp;currency_code=USD&amp;bn=PP-DonationsBF&amp;charset=UTF-8" title="Donate via PayPal"><?php _e('drop in your contribution','ald_bsearch_plugin'); ?></a>.
-	  (<a href="http://ajaydsouza.com/donate/"><?php _e('Some reasons why you should.','ald_bsearch_plugin'); ?></a>)</p>
+      <?php _e('useful, please do', BSEARCH_LOCAL_NAME); ?>
+      <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&amp;business=donate@ajaydsouza.com&amp;item_name=Better%20Search%20(From%20WP-Admin)&amp;no_shipping=1&amp;return=http://ajaydsouza.com/wordpress/plugins/better-search/&amp;cancel_return=http://ajaydsouza.com/wordpress/plugins/better-search/&amp;cn=Note%20to%20Author&amp;tax=0&amp;currency_code=USD&amp;bn=PP-DonationsBF&amp;charset=UTF-8" title="Donate via PayPal"><?php _e('drop in your contribution', BSEARCH_LOCAL_NAME); ?></a>.
+	  (<a href="http://ajaydsouza.com/donate/"><?php _e('Some reasons why you should.', BSEARCH_LOCAL_NAME); ?></a>)</p>
     </fieldset>
   </div>
   <form method="post" id="bsearch_options" name="bsearch_options" style="border: #ccc 1px solid; padding: 10px">
     <fieldset class="options">
     <legend>
     <h3>
-      <?php _e('Options:','ald_bsearch_plugin'); ?>
+      <?php _e('Options:', BSEARCH_LOCAL_NAME); ?>
     </h3>
     </legend>
     <p>
       <label>
-      <?php _e('Number of Search Results per page: ','ald_bsearch_plugin'); ?>
+      <?php _e('Number of Search Results per page: ', BSEARCH_LOCAL_NAME); ?>
       <input type="textbox" name="limit" id="limit" value="<?php echo stripslashes($bsearch_settings[limit]); ?>">
       </label>
     </p>
     <p>
       <label>
-      <?php _e('Title of Overall Popular Searchs: ','ald_bsearch_plugin'); ?>
+      <?php _e('Title of Overall Popular Searchs: ', BSEARCH_LOCAL_NAME); ?>
       <input type="textbox" name="title" id="title" value="<?php echo stripslashes($bsearch_settings[title]); ?>">
       </label>
     </p>
     <p>
       <label>
-      <?php _e('Title of Daily Popular Searches: ','ald_bsearch_plugin'); ?>
+      <?php _e('Title of Daily Popular Searches: ', BSEARCH_LOCAL_NAME); ?>
       <input type="textbox" name="title_daily" id="title_daily" value="<?php echo stripslashes($bsearch_settings[title_daily]); ?>">
       </label>
     </p>
     <p>
       <label>
-      <?php _e('Daily Popular should contain searches of how many days? ','ald_bsearch_plugin'); ?>
+      <?php _e('Daily Popular should contain searches of how many days? ', BSEARCH_LOCAL_NAME); ?>
       <input type="textbox" name="daily_range" id="daily_range" size="3" value="<?php echo stripslashes($bsearch_settings[daily_range]); ?>">
       </label>
     </p>
     <p>
       <label>
+      <input type="checkbox" name="include_pages" id="include_pages" <?php if ($bsearch_settings[include_pages]) echo 'checked="checked"' ?> />
+      <?php _e('Include WordPress static pages in Search Results', BSEARCH_LOCAL_NAME); ?>
+      </label>
+    </p>
+    <p>
+      <label>
       <input type="checkbox" name="d_use_js" id="d_use_js" <?php if ($bsearch_settings[d_use_js]) echo 'checked="checked"' ?> />
-      <?php _e('Bypass Cache for daily popular searches\' heatmap? This options uses JavaScript to load the post and can increase your page load time','ald_bsearch_plugin'); ?>
+      <?php _e('Bypass Cache for daily popular searches\' heatmap? This options uses JavaScript to load the post and can increase your page load time', BSEARCH_LOCAL_NAME); ?>
       </label>
     </p>
     <p>
       <label>
       <input type="checkbox" name="show_credit" id="show_credit" <?php if ($bsearch_settings[show_credit]) echo 'checked="checked"' ?> />
-      <?php _e('A link to the plugin is added as an extra list item to the list of popular searches. Not mandatory, but thanks if you do it!','ald_bsearch_plugin'); ?>
+      <?php _e('A link to the plugin is added as an extra list item to the list of popular searches. Not mandatory, but thanks if you do it!', BSEARCH_LOCAL_NAME); ?>
       </label>
     </p>
 	</fieldset>
     <fieldset class="options">
     <legend>
     <h3>
-      <?php _e('Heatmap (Popular searches) Options:','ald_bsearch_plugin'); ?>
+      <?php _e('Heatmap (Popular searches) Options:', BSEARCH_LOCAL_NAME); ?>
     </h3>
     </legend>
     <p>
       <label>
-      <?php _e('Number of search terms to display: ','ald_bsearch_plugin'); ?>
+      <?php _e('Number of search terms to display: ', BSEARCH_LOCAL_NAME); ?>
       <input type="textbox" name="heatmap_limit" id="heatmap_limit" value="<?php echo stripslashes($bsearch_settings[heatmap_limit]); ?>">
       </label>
     </p>
     <p>
       <label>
-      <?php _e('Font size of least popular search term: ','ald_bsearch_plugin'); ?>
+      <?php _e('Font size of least popular search term: ', BSEARCH_LOCAL_NAME); ?>
       <input type="textbox" name="heatmap_smallest" id="heatmap_smallest" value="<?php echo stripslashes($bsearch_settings[heatmap_smallest]); ?>">
       </label>
     </p>
     <p>
       <label>
-      <?php _e('Font size of most popular search term: ','ald_bsearch_plugin'); ?>
+      <?php _e('Font size of most popular search term: ', BSEARCH_LOCAL_NAME); ?>
       <input type="textbox" name="heatmap_largest" id="heatmap_largest" value="<?php echo stripslashes($bsearch_settings[heatmap_largest]); ?>">
       </label>
     </p>
     <p>
       <label>
-      <?php _e('Color of least popular search term: ','ald_bsearch_plugin'); ?>
+      <?php _e('Color of least popular search term: ', BSEARCH_LOCAL_NAME); ?>
       <input type="textbox" class="color" name="heatmap_cold" id="heatmap_cold" value="<?php echo stripslashes($bsearch_settings[heatmap_cold]); ?>">
       </label>
     </p>
     <p>
       <label>
-      <?php _e('Color of most popular search term: ','ald_bsearch_plugin'); ?>
+      <?php _e('Color of most popular search term: ', BSEARCH_LOCAL_NAME); ?>
       <input type="textbox" class="color" name="heatmap_hot" id="heatmap_hot" value="<?php echo stripslashes($bsearch_settings[heatmap_hot]); ?>">
       </label>
     </p>
     <p>
       <label>
-      <?php _e('Text to include before each search term in heatmap','ald_bsearch_plugin'); ?>
+      <?php _e('Text to include before each search term in heatmap', BSEARCH_LOCAL_NAME); ?>
       <input type="textbox" name="heatmap_before" id="heatmap_before" size="3" value="<?php echo stripslashes($bsearch_settings[heatmap_before]); ?>">
       </label>
     </p>
     <p>
       <label>
-      <?php _e('Text to include after each search term in heatmap','ald_bsearch_plugin'); ?>
+      <?php _e('Text to include after each search term in heatmap', BSEARCH_LOCAL_NAME); ?>
       <input type="textbox" name="heatmap_after" id="heatmap_after" size="3" value="<?php echo stripslashes($bsearch_settings[heatmap_after]); ?>">
       </label>
     </p>
     </fieldset>
     <p>
       <input type="submit" name="bsearch_save" id="bsearch_save" value="Save Options" style="border:#00CC00 1px solid" />
-      <input name="bsearch_default" type="submit" id="bsearch_default" value="Default Options" style="border:#FF0000 1px solid" onclick="if (!confirm('<?php _e('Do you want to set options to Default?','ald_bsearch_plugin'); ?>')) return false;" />
+      <input name="bsearch_default" type="submit" id="bsearch_default" value="Default Options" style="border:#FF0000 1px solid" onclick="if (!confirm('<?php _e('Do you want to set options to Default?', BSEARCH_LOCAL_NAME); ?>')) return false;" />
     </p>
   </form>
 </div>
@@ -187,7 +198,7 @@ function bsearch_adminmenu() {
 	}
 
 	if ((function_exists('add_options_page'))&&($bsearch_is_admin)) {
-		$plugin_page = add_options_page(__("Better Search", 'myald_bsearch_plugin'), __("Better Search", 'myald_bsearch_plugin'), 9, 'bsearch_options', 'bsearch_options');
+		$plugin_page = add_options_page(__("Better Search", BSEARCH_LOCAL_NAME), __("Better Search", BSEARCH_LOCAL_NAME), 9, 'bsearch_options', 'bsearch_options');
 		add_action( 'admin_head-'. $plugin_page, 'bsearch_adminhead' );
 	}
 }
@@ -214,7 +225,7 @@ function bsearch_pop_dashboard() {
 	$before = $bsearch_settings[heatmap_before];
 	$after = $bsearch_settings[heatmap_after];
 
-	echo bsearch_heatmap(false, $smallest, $largest, $unit, $cold, $hot, $before, $after, '', $limit);
+	echo get_bsearch_heatmap(false, $smallest, $largest, $unit, $cold, $hot, $before, $after, '', $limit);
 	
 	if ($bsearch_settings['show_credit']) echo '<br /><small>Powered by <a href="http://ajaydsouza.com/wordpress/plugins/better-search/">Better Search plugin</a></small>';
 }
@@ -230,15 +241,15 @@ function bsearch_pop_daily_dashboard() {
 	$before = $bsearch_settings[heatmap_before];
 	$after = $bsearch_settings[heatmap_after];
 
-	echo bsearch_heatmap(true, $smallest, $largest, $unit, $cold, $hot, $before, $after, '', $limit);
+	echo get_bsearch_heatmap(true, $smallest, $largest, $unit, $cold, $hot, $before, $after, '', $limit);
 	
 	if ($bsearch_settings['show_credit']) echo '<br /><small>Powered by <a href="http://ajaydsouza.com/wordpress/plugins/better-search/">Better Search plugin</a></small>';
 }
  
 function bsearch_pop_dashboard_setup() {
 	if (function_exists('wp_add_dashboard_widget')) {
-		wp_add_dashboard_widget( 'bsearch_pop_dashboard', __( 'Popular Searches' ), 'bsearch_pop_dashboard' );
-		wp_add_dashboard_widget( 'bsearch_pop_daily_dashboard', __( 'Daily Popular Searches' ), 'bsearch_pop_daily_dashboard' );
+		wp_add_dashboard_widget( 'bsearch_pop_dashboard', __( 'Popular Searches', BSEARCH_LOCAL_NAME ), 'bsearch_pop_dashboard' );
+		wp_add_dashboard_widget( 'bsearch_pop_daily_dashboard', __( 'Daily Popular Searches', BSEARCH_LOCAL_NAME ), 'bsearch_pop_daily_dashboard' );
 	}
 }
 add_action('wp_dashboard_setup', 'bsearch_pop_dashboard_setup');
---
+++ better-search-template.php
@@ -0,0 +1,34 @@
+﻿<?php /* Sample template for Better Search Plugin for WordPress Default theme */
+
+	get_header(); ?>
+	<div id="content" class="narrowcolumn">
+
+	<div id="heatmap" style="padding: 5px; border: 1px dashed #ccc">
+	<div style="padding: 5px; border-bottom: 1px dashed #ccc">
+	<h2>
+	<?php echo get_bsearch_title_daily(); ?>
+	</h2>
+	<?php echo get_bsearch_heatmap(true); ?>
+	</div>
+	<div style="padding: 5px;">
+	<h2>
+	<?php echo get_bsearch_title(); ?>
+	</h2>
+	<?php echo get_bsearch_heatmap(false); ?>
+	</div>
+	<div style="clear:both">&nbsp;</div>
+	</div>
+
+	<div style="padding: 5px;margin: 5px;">
+	<?php echo get_bsearch_form($s); ?>
+	</div>
+
+	<div id="searchresults"><h2 class="pagetitle">Search Results for:	&quot;<?php echo $s; ?>&quot;</h2>
+
+	<?php bsearch_results($s,$limit); ?>
+
+	</div>
+	</div>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
