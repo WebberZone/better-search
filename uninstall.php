b19eb66847c2f598fde44676976827032e399bbb
Ajay <Ajay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-06 17:31:38 +0000

    git-svn-id: http://plugins.svn.wordpress.org/better-search/trunk@143448 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -9,6 +9,19 @@ if ( !defined('ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') ) {
    	$table_name = $wpdb->prefix . "bsearch_daily";
 	$sql = "DROP TABLE $table_name";
 	$wpdb->query($sql);
+	
+    $poststable = $wpdb->posts;
+
+	$sql = "ALTER TABLE $poststable DROP INDEX bsearch";
+	$wpdb->query($sql);
+	
+	$sql = "ALTER TABLE $poststable DROP INDEX bsearch_title";
+	$wpdb->query($sql);
+	
+	$sql = "ALTER TABLE $poststable DROP INDEX bsearch_content";
+	$wpdb->query($sql);
+	
+	
 	delete_option('ald_bsearch_settings');
 	delete_option('bsearch_db_version');
 ?>
