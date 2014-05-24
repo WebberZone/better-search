532052b4347271e0557abc0445f4927bb0d6f2c1
Ajay D'Souza <me@ajaydsouza.com>
2014-05-24 10:37:06 +0100

    Updated uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,15 +1,32 @@
 <?php
-if ( !defined('ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') ) {
-    exit();
+/**
+ * Fired when the plugin is uninstalled
+ *
+ * @package BSearch
+ */
+
+// If uninstall not called from WordPress, then exit
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
 }
-	global $wpdb;
-   	$table_name = $wpdb->prefix . "bsearch";
-	$sql = "DROP TABLE $table_name";
-	$wpdb->query($sql);
-   	$table_name = $wpdb->prefix . "bsearch_daily";
-	$sql = "DROP TABLE $table_name";
-	$wpdb->query($sql);
-    $wpdb->query('ALTER TABLE '.$wpdb->posts.' DROP INDEX bsearch ;');
-	delete_option('ald_bsearch_settings');
-	delete_option('bsearch_db_version');
+
+global $wpdb;
+
+// Delete Better Search table
+$table_name = $wpdb->prefix . "bsearch";
+$sql = "DROP TABLE $table_name";
+$wpdb->query( $sql );
+
+// Delete Better Daily Search table
+$table_name = $wpdb->prefix . "bsearch_daily";
+$sql = "DROP TABLE $table_name";
+$wpdb->query( $sql );
+
+// Drop FULLTEXT index
+$wpdb->query( 'ALTER TABLE '.$wpdb->posts.' DROP INDEX bsearch ;' );
+
+// Delete plugin options
+delete_option('ald_bsearch_settings');
+delete_option('bsearch_db_version');
+
 ?>
