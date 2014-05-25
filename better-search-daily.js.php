edbf4840be18d3bf47997e606be776f0d29f3e91
Ajay D'Souza <me@ajaydsouza.com>
2014-05-25 10:22:49 +0100

    Cleaned up .js.php files

--- better-search-daily.js.php
+++ better-search-daily.js.php
@@ -1,49 +1,61 @@
 <?php
-//"better-search-daily.js.php" Display Daily Popular Lists.
-Header("content-type: application/x-javascript");
+/**
+ * Display Daily Popular Search terms.
+ *
+ * @package BSearch
+ */
 
-if (!function_exists('add_action')) {
-	$wp_root = '../../..';
-	if (file_exists($wp_root.'/wp-load.php')) {
-		require_once($wp_root.'/wp-load.php');
-	} else {
-		require_once($wp_root.'/wp-config.php');
-	}
+Header( "content-type: application/x-javascript" );
+
+// Force a short-init since we just need core WP, not the entire framework stack
+//define( 'SHORTINIT', true );
+
+// Build the wp-load.php path from a plugin/theme
+$wp_load_path = dirname( dirname( dirname( __FILE__ ) ) );
+// Require the wp-load.php file (which loads wp-config.php and bootstraps WordPress)
+$wp_load_filename = '/wp-load.php';
+
+// Check if the file exists in the root or one level up
+if( ! file_exists( $wp_load_path . $wp_load_filename ) ) {
+    // Just in case the user may have placed wp-config.php one more level up from the root
+    $wp_load_filename = dirname( $wp_load_path ) . $wp_load_filename;
 }
+// Require the wp-config.php file
+require( $wp_load_filename );
 
-// Display counter using Ajax
+
+/**
+ * Display heatmap of top results using Ajax.
+ *
+ */
 function bsearch_daily_searches() {
-	global $wpdb, $siteurl, $tableposts, $s;
+	global $wpdb, $siteurl, $tableposts, $search_query;
 	$table_name = $wpdb->prefix . "bsearch_daily";
-	
-	$is_widget = intval($_GET['widget']);
-	
+
+	$is_widget = intval( $_GET['widget'] );
+
 	$bsearch_settings = bsearch_read_options();
-	$limit = $bsearch_settings[heatmap_limit];
-	$largest = intval($bsearch_settings[heatmap_largest]);
-	$smallest = intval($bsearch_settings[heatmap_smallest]);
-	$hot = $bsearch_settings[heatmap_hot];
-	$cold = $bsearch_settings[heatmap_cold];
-	$unit = $bsearch_settings[heatmap_unit];
-	$before = $bsearch_settings[heatmap_before];
-	$after = $bsearch_settings[heatmap_after];
 
 	$output = '';
-	
-	if(!$is_widget) {
-		$output .= '<div class="bsearch_heatmap">';	
+
+	$output .= '<div class="bsearch_heatmap">';
+
+	if ( ! $is_widget ) {
 		$output .= $bsearch_settings['title_daily'];
 	}
-	
-	$output .= '<div text-align:center>'.get_bsearch_heatmap(true, $smallest, $largest, $unit, $cold, $hot, $before, $after, '',$limit).'</div>';
-		
-	if ($bsearch_settings['show_credit']) $output .= '<br /><small>Powered by <a href="http://ajaydsouza.com/wordpress/plugins/better-search/">Better Search plugin</a></small>';
-		
-	if(!$is_widget) {
-		$output .= '</div>';
+
+	$output .= '<div text-align:center>';
+	$output .= get_bsearch_heatmap( array(
+		'daily' => 1,
+	) );
+	$output .= '</div>';
+
+	if ( $bsearch_settings['show_credit'] ) {
+		$output .= '<br /><small>Powered by <a href="http://ajaydsouza.com/wordpress/plugins/better-search/">Better Search plugin</a></small>';
 	}
-	
-	echo "document.write('".$output."')";
+	$output .= '</div>';
+
+	echo "document.write('" . $output . "')";
 }
 bsearch_daily_searches();
 ?>
