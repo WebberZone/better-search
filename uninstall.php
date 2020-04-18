1af6094d0eec09cd03c9046f88606ef17b1c26d6
Ajay DSouza <me@ajaydsouza.com>
2020-04-18 19:17:58 +0100

    Refactor uninstall.php code

--- uninstall.php
+++ uninstall.php
@@ -14,7 +14,33 @@ global $wpdb;
 
 $bsearch_settings = get_option( 'ald_bsearch_settings' );
 
-if ( ! is_multisite() ) {
+if ( is_multisite() ) {
+
+	// Get all blogs in the network and activate plugin on each one.
+	$blog_ids = $wpdb->get_col( //phpcs:ignore WordPress.DB.DirectDatabaseQuery.SchemaChange,WordPress.DB.DirectDatabaseQuery.NoCaching,WordPress.DB.DirectDatabaseQuery.DirectQuery
+		"
+        SELECT blog_id FROM $wpdb->blogs
+        WHERE archived = '0' AND spam = '0' AND deleted = '0'
+	"
+	);
+
+	foreach ( $blog_ids as $blogid ) {
+		switch_to_blog( $blogid );
+		bsearch_delete_data();
+		restore_current_blog();
+	}
+} else {
+	bsearch_delete_data();
+}
+
+
+/**
+ * Delete plugin data.
+ *
+ * @since 2.5.0
+ */
+function bsearch_delete_data() {
+	global $wpdb;
 
 	$wpdb->query( 'DROP TABLE ' . $wpdb->prefix . 'bsearch' ); //phpcs:ignore WordPress.DB.DirectDatabaseQuery.SchemaChange,WordPress.DB.DirectDatabaseQuery.NoCaching,WordPress.DB.DirectDatabaseQuery.DirectQuery
 	$wpdb->query( 'DROP TABLE ' . $wpdb->prefix . 'bsearch_daily' ); //phpcs:ignore WordPress.DB.DirectDatabaseQuery.SchemaChange,WordPress.DB.DirectDatabaseQuery.NoCaching,WordPress.DB.DirectDatabaseQuery.DirectQuery
@@ -26,35 +52,4 @@ if ( ! is_multisite() ) {
 	$wpdb->query( 'ALTER TABLE ' . $wpdb->posts . ' DROP INDEX bsearch_content' ); //phpcs:ignore WordPress.DB.DirectDatabaseQuery.SchemaChange,WordPress.DB.DirectDatabaseQuery.NoCaching,WordPress.DB.DirectDatabaseQuery.DirectQuery
 
 	delete_option( 'bsearch_db_version' );
-
-} else {
-
-	// Get all blogs in the network and activate plugin on each one.
-	$blog_ids = $wpdb->get_col( //phpcs:ignore WordPress.DB.DirectDatabaseQuery.SchemaChange,WordPress.DB.DirectDatabaseQuery.NoCaching,WordPress.DB.DirectDatabaseQuery.DirectQuery
-		"
-        SELECT blog_id FROM $wpdb->blogs
-        WHERE archived = '0' AND spam = '0' AND deleted = '0'
-	"
-	);
-
-	foreach ( $blog_ids as $blogid ) {
-
-		switch_to_blog( $blogid );
-
-		$wpdb->query( 'DROP TABLE ' . $wpdb->prefix . 'bsearch' ); //phpcs:ignore WordPress.DB.DirectDatabaseQuery.SchemaChange,WordPress.DB.DirectDatabaseQuery.NoCaching,WordPress.DB.DirectDatabaseQuery.DirectQuery
-		$wpdb->query( 'DROP TABLE ' . $wpdb->prefix . 'bsearch_daily' ); //phpcs:ignore WordPress.DB.DirectDatabaseQuery.SchemaChange,WordPress.DB.DirectDatabaseQuery.NoCaching,WordPress.DB.DirectDatabaseQuery.DirectQuery
-
-		delete_option( 'ald_bsearch_settings' );
-
-		$wpdb->query( 'ALTER TABLE ' . $wpdb->posts . ' DROP INDEX bsearch' ); //phpcs:ignore WordPress.DB.DirectDatabaseQuery.SchemaChange,WordPress.DB.DirectDatabaseQuery.NoCaching,WordPress.DB.DirectDatabaseQuery.DirectQuery
-		$wpdb->query( 'ALTER TABLE ' . $wpdb->posts . ' DROP INDEX bsearch_title' ); //phpcs:ignore WordPress.DB.DirectDatabaseQuery.SchemaChange,WordPress.DB.DirectDatabaseQuery.NoCaching,WordPress.DB.DirectDatabaseQuery.DirectQuery
-		$wpdb->query( 'ALTER TABLE ' . $wpdb->posts . ' DROP INDEX bsearch_content' ); //phpcs:ignore WordPress.DB.DirectDatabaseQuery.SchemaChange,WordPress.DB.DirectDatabaseQuery.NoCaching,WordPress.DB.DirectDatabaseQuery.DirectQuery
-
-		delete_option( 'bsearch_db_version' );
-
-	}
-
-	// Switch back to the current blog.
-	restore_current_blog();
-
 }
