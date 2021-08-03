b73d28b8d20a5c2efdb7ee6e6351b2830ff6ffdf
Ajay DSouza <me@ajaydsouza.com>
2021-08-03 11:42:31 +0100

    Uninstalling didn't delete the option key from database.

--- better-search.php
+++ better-search.php
@@ -9,13 +9,13 @@
  * @author    Ajay D'Souza <me@ajaydsouza.com>
  * @license   GPL-2.0+
  * @link      https://webberzone.com
- * @copyright 2009-2020 Ajay D'Souza
+ * @copyright 2009-2021 Ajay D'Souza
  *
  * @wordpress-plugin
  * Plugin Name: Better Search
  * Plugin URI:  https://webberzone.com/plugins/better-search/
  * Description: Replace the default WordPress search with a contextual search. Search results are sorted by relevancy ensuring a better visitor search experience.
- * Version:     2.5.4-beta1
+ * Version:     3.0.0-beta1
  * Author:      Ajay D'Souza
  * Author URI:  https://webberzone.com/
  * Text Domain: better-search
