1a1fa72c22462dbc7d51d20366bc5e2c84fb9581
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2024-09-20 20:53:51 +0100

    Updated better-search-core-query

--- includes/class-better-search-query.php
+++ includes/class-better-search-query.php
@@ -28,34 +28,35 @@ if ( ! class_exists( 'Better_Search_Query' ) ) :
 		 * @param array|string $args The Query variables. Accepts an array or a query string.
 		 */
 		public function __construct( $args = array() ) {
-			$better_search = new Better_Search_Core_Query( $args );
+			$args       = wp_parse_args( $args, array( 'is_better_search_query' => true ) );
+			$core_query = new Better_Search_Core_Query( $args );
 
-			add_filter( 'pre_get_posts', array( $better_search, 'pre_get_posts' ), 10 );
-			add_filter( 'posts_fields', array( $better_search, 'posts_fields' ), 10, 2 );
-			add_filter( 'posts_join', array( $better_search, 'posts_join' ), 10, 2 );
-			add_filter( 'posts_search', array( $better_search, 'posts_search' ), 10, 2 );
-			add_filter( 'posts_where', array( $better_search, 'posts_where' ), 10, 2 );
-			add_filter( 'posts_distinct', array( $better_search, 'posts_distinct' ), 10, 2 );
-			add_filter( 'posts_orderby', array( $better_search, 'posts_orderby' ), 10, 2 );
-			add_filter( 'posts_groupby', array( $better_search, 'posts_groupby' ), 10, 2 );
-			add_filter( 'posts_clauses_request', array( $better_search, 'set_topscore' ), 10, 2 );
-			add_filter( 'posts_pre_query', array( $better_search, 'posts_pre_query' ), 10, 2 );
-			add_filter( 'the_posts', array( $better_search, 'the_posts' ), 10, 2 );
+			add_filter( 'pre_get_posts', array( $core_query, 'pre_get_posts' ), 10 );
+			add_filter( 'posts_fields', array( $core_query, 'posts_fields' ), 10, 2 );
+			add_filter( 'posts_join', array( $core_query, 'posts_join' ), 10, 2 );
+			add_filter( 'posts_search', array( $core_query, 'posts_search' ), 10, 2 );
+			add_filter( 'posts_where', array( $core_query, 'posts_where' ), 10, 2 );
+			add_filter( 'posts_distinct', array( $core_query, 'posts_distinct' ), 10, 2 );
+			add_filter( 'posts_orderby', array( $core_query, 'posts_orderby' ), 10, 2 );
+			add_filter( 'posts_groupby', array( $core_query, 'posts_groupby' ), 10, 2 );
+			add_filter( 'posts_clauses_request', array( $core_query, 'set_topscore' ), 10, 2 );
+			add_filter( 'posts_pre_query', array( $core_query, 'posts_pre_query' ), 10, 2 );
+			add_filter( 'the_posts', array( $core_query, 'the_posts' ), 10, 2 );
 
-			parent::__construct( $better_search->query_args );
+			parent::__construct( $core_query->query_args );
 
 			// Remove filters after use.
-			remove_filter( 'pre_get_posts', array( $better_search, 'pre_get_posts' ) );
-			remove_filter( 'posts_fields', array( $better_search, 'posts_fields' ) );
-			remove_filter( 'posts_join', array( $better_search, 'posts_join' ) );
-			remove_filter( 'posts_search', array( $better_search, 'posts_search' ) );
-			remove_filter( 'posts_where', array( $better_search, 'posts_where' ) );
-			remove_filter( 'posts_distinct', array( $better_search, 'posts_distinct' ) );
-			remove_filter( 'posts_orderby', array( $better_search, 'posts_orderby' ) );
-			remove_filter( 'posts_groupby', array( $better_search, 'posts_groupby' ) );
-			remove_filter( 'posts_clauses_request', array( $better_search, 'set_topscore' ) );
-			remove_filter( 'posts_pre_query', array( $better_search, 'posts_pre_query' ) );
-			remove_filter( 'the_posts', array( $better_search, 'the_posts' ) );
+			remove_filter( 'pre_get_posts', array( $core_query, 'pre_get_posts' ) );
+			remove_filter( 'posts_fields', array( $core_query, 'posts_fields' ) );
+			remove_filter( 'posts_join', array( $core_query, 'posts_join' ) );
+			remove_filter( 'posts_search', array( $core_query, 'posts_search' ) );
+			remove_filter( 'posts_where', array( $core_query, 'posts_where' ) );
+			remove_filter( 'posts_distinct', array( $core_query, 'posts_distinct' ) );
+			remove_filter( 'posts_orderby', array( $core_query, 'posts_orderby' ) );
+			remove_filter( 'posts_groupby', array( $core_query, 'posts_groupby' ) );
+			remove_filter( 'posts_clauses_request', array( $core_query, 'set_topscore' ) );
+			remove_filter( 'posts_pre_query', array( $core_query, 'posts_pre_query' ) );
+			remove_filter( 'the_posts', array( $core_query, 'the_posts' ) );
 		}
 	}
 endif;
