06af159a69f547dff57ce929d17d3e03385fd375
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2024-11-19 23:18:27 +0000

    Word fixes. v4.0.0-RC2

--- includes/admin/settings/class-metabox-api.php
+++ includes/admin/settings/class-metabox-api.php
@@ -298,7 +298,7 @@ class Metabox_API {
 		echo '</table>';
 
 		/**
-		 * Action triggered when displaying Top 10 meta box.
+		 * Action triggered when displaying Better Search meta box.
 		 *
 		 * @param object $post  Post object.
 		 */
