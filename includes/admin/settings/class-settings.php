322d7ff23e97f7f3efd574f4143dfddde04f9e98
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2024-11-29 20:06:45 +0000

    Change pro upgrade link

--- includes/admin/settings/class-settings.php
+++ includes/admin/settings/class-settings.php
@@ -12,6 +12,8 @@ namespace WebberZone\Better_Search\Admin\Settings;
 
 use WebberZone\Better_Search\Util\Helpers;
 
+use function WebberZone\Better_Search\bsearch_freemius;
+
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
 	die;
@@ -1131,7 +1133,7 @@ class Settings {
 		if ( isset( $args['pro'] ) && $args['pro'] ) {
 			$output .= sprintf(
 				'<a class="bsearch_button bsearch_button_gold" target="_blank" href="%s" title="%s">%s</a>',
-				esc_url( 'https://webberzone.com/plugins/better-search/pro/' ),
+				esc_url( bsearch_freemius()->get_upgrade_url() ),
 				esc_attr__( 'Upgrade to Pro', 'better-search' ),
 				esc_html__( 'Upgrade to Pro', 'better-search' )
 			);
