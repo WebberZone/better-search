a723db0a108d1f35eec8ded33580613ea3f9a9d2
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2025-05-11 16:35:51 +0100

    Update load-freemius.php

--- includes/load-freemius.php
+++ includes/load-freemius.php
@@ -1,48 +1,50 @@
 <?php
-
 /**
  * Loads Freemius SDK.
  *
  * @package WebberZone\Better_Search
  */
+
 namespace WebberZone\Better_Search;
 
-if ( !defined( 'ABSPATH' ) ) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 /**
  * Initialize Freemius SDK.
  */
 function bsearch_freemius() {
-    global $bsearch_freemius;
-    if ( !isset( $bsearch_freemius ) ) {
-        if ( !defined( 'WP_FS__PRODUCT_17020_MULTISITE' ) ) {
-            define( 'WP_FS__PRODUCT_17020_MULTISITE', true );
-        }
-        // Include Freemius SDK.
-        require_once plugin_dir_path( __DIR__ ) . 'freemius/start.php';
-        $bsearch_freemius = \fs_dynamic_init( array(
-            'id'             => '17020',
-            'slug'           => 'better-search',
-            'premium_slug'   => 'better-search-pro',
-            'type'           => 'plugin',
-            'public_key'     => 'pk_40525301bca835d9836ec4d946693',
-            'is_premium'     => true,
-            'premium_suffix' => 'Pro',
-            'has_addons'     => false,
-            'has_paid_plans' => true,
-            'menu'           => array(
-                'slug'    => 'bsearch_dashboard',
-                'contact' => false,
-                'support' => false,
-                'network' => true,
-            ),
-            'is_live'        => true,
-        ) );
-    }
-    $bsearch_freemius->add_filter( 'plugin_icon', __NAMESPACE__ . '\\bsearch_freemius_get_plugin_icon' );
-    $bsearch_freemius->add_filter( 'after_uninstall', __NAMESPACE__ . '\\bsearch_freemius_uninstall' );
-    return $bsearch_freemius;
+	global $bsearch_freemius;
+	if ( ! isset( $bsearch_freemius ) ) {
+		if ( ! defined( 'WP_FS__PRODUCT_17020_MULTISITE' ) ) {
+			define( 'WP_FS__PRODUCT_17020_MULTISITE', true );
+		}
+		// Include Freemius SDK.
+		require_once plugin_dir_path( __DIR__ ) . 'freemius/start.php';
+		$bsearch_freemius = \fs_dynamic_init(
+			array(
+				'id'             => '17020',
+				'slug'           => 'better-search',
+				'premium_slug'   => 'better-search-pro',
+				'type'           => 'plugin',
+				'public_key'     => 'pk_40525301bca835d9836ec4d946693',
+				'is_premium'     => true,
+				'premium_suffix' => 'Pro',
+				'has_addons'     => false,
+				'has_paid_plans' => true,
+				'menu'           => array(
+					'slug'    => 'bsearch_dashboard',
+					'contact' => false,
+					'support' => false,
+					'network' => true,
+				),
+				'is_live'        => true,
+			)
+		);
+	}
+	$bsearch_freemius->add_filter( 'plugin_icon', __NAMESPACE__ . '\\bsearch_freemius_get_plugin_icon' );
+	$bsearch_freemius->add_filter( 'after_uninstall', __NAMESPACE__ . '\\bsearch_freemius_uninstall' );
+	return $bsearch_freemius;
 }
 
 /**
@@ -51,17 +53,17 @@ function bsearch_freemius() {
  * @return string
  */
 function bsearch_freemius_get_plugin_icon() {
-    return __DIR__ . '/admin/images/bsearch-icon.png';
+	return __DIR__ . '/admin/images/bsearch-icon.png';
 }
 
 /**
  * Uninstall the plugin.
  */
 function bsearch_freemius_uninstall() {
-    require_once dirname( __DIR__ ) . '/uninstaller.php';
-    if ( bsearch_freemius()->can_use_premium_code__premium_only() ) {
-        \WebberZone\Better_Search\Pro\Pro::uninstall_pro();
-    }
+	require_once dirname( __DIR__ ) . '/uninstaller.php';
+	if ( bsearch_freemius()->can_use_premium_code__premium_only() ) {
+		\WebberZone\Better_Search\Pro\Pro::uninstall_pro();
+	}
 }
 
 // Init Freemius.
