1b777fc0cbff6083df2a200f7002ab182c84f921
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2026-04-22 22:36:53 +0400

    Fix: Escape output in Settings API - use wp_kses, replace script template with HTML template element
    - Replace unescaped echo with wp_kses_post() in Settings_API::show_navigation()
    - Switch repeater template from <script type="text/template"> to <template> element
    - Update JS to use .get(0).innerHTML for <template> content retrieval
    - Use wp_kses() with get_allowed_html() for repeater output
    - Use wp_kses_post() for WYSIWYG field description
    - Inline aria-current attribute in wizard step navigation
    - Add <template> to allowed HTML tags in get_allowed_html()

--- includes/admin/settings/class-settings-api.php
+++ includes/admin/settings/class-settings-api.php
@@ -5,7 +5,7 @@
  * Functions to register, read, write and update settings.
  * Portions of this code have been inspired by Easy Digital Downloads, WordPress Settings Sandbox, WordPress Settings API class, etc.
  *
- * @package WebberZone\Better_Search
+ * @package    Better_Search
  */
 
 namespace WebberZone\Better_Search\Admin\Settings;
@@ -997,7 +997,7 @@ class Settings_API {
 
 		$html .= '</ul>';
 
-		echo $html; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo wp_kses_post( $html );
 	}
 
 	/**
